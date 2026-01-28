18bbff86815d6772eee6a9a473597bf6407bd321
Florentin Garnier <florentin@garnier.tech>
2026-01-28 12:08:38 +0100

    **feat(front): enrichir l'utilisation des contrôleurs Stimulus et améliorer les templates Twig 🖼️**
    - Enregistrement de nouveaux contrôleurs Stimulus : `csrf-protection`, `player-card-assigner`, `sortable` et `dashboard`.
    - Simplification des blocs Twig pour une meilleure lisibilité dans `dashboard.html.twig` et `_layout.html.twig`.

--- assets/stimulus_bootstrap.js
+++ assets/stimulus_bootstrap.js
@@ -1,5 +1,14 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
 
 const app = startStimulusApp();
-// register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
+
+// Import custom controllers
+import CsrfProtectionController from './controllers/csrf_protection_controller.js';
+import PlayerCardAssignerController from './controllers/player_card_assigner_controller.js';
+import SortableController from './controllers/sortable_controller.js';
+import DashboardController from './controllers/dashboard_controller.js';
+
+app.register('csrf-protection', CsrfProtectionController);
+app.register('player-card-assigner', PlayerCardAssignerController);
+app.register('sortable', SortableController);
+app.register('dashboard', DashboardController);
